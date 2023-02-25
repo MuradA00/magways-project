312d03c60f8dd6858bedbc1e62cda9150b8e1e71
Murad <abishlimurad@gmail.com>
2023-02-25 21:47:05 +0200

    first commit

---
+++ source/js/common.js
@@ -0,0 +1,17 @@
+const reviews = new Swiper('.reviews__wrapper', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 30,
+  breakpoints: {
+    950: {
+      slidesPerView: 3
+    },
+    768: {
+      slidesPerView: 2
+    }
+  },
+  navigation: {
+    nextEl: '.reviews__arrow_next',
+    prevEl: '.reviews__arrow_prev'
+  }
+})
